90daae69a7defe7276112f9d192000f9a9192c3d
Shyplants <wlsdn1004100@naver.com>
2022-05-05 00:09:29 +0900

    UPD

--- 2021/2차대회/고등부/헬기 착륙장.cpp
+++ 2021/2차대회/고등부/헬기 착륙장.cpp
@@ -6,8 +6,9 @@ const int MOD = 1e9 + 7;
 const int MAX_R = 450;
 const int MAX_COLOR = 5e4 + 4;
 
-int dp[MAX_R][MAX_COLOR];
-int pSum[MAX_R][MAX_COLOR];
+int dp[MAX_R][MAX_COLOR];    // dp[i][j] = 가장 바깥원의 반지름이 i이고 파랑 페인트의 양을 j만큼 사용하여 나타낼 수 있는 경우의 수
+                             //            가장 바깥원의 반지름i 그릴때 필요한 페인트(레드+블루) 총량 : i*(i+1)/2, 파랑 페인트의 양이 j일때 빨간 페인트의 양은 i*(i+1)/2 - j
+int pSum[MAX_R][MAX_COLOR];  // pSum[i][j] = sum(dp[i][0] ... dp[i][j])
 
 int main()
 {
@@ -18,13 +19,13 @@ int main()
 
 	dp[0][0] = 1;
 
-	for(int i=1; i < MAX_R; ++i) {
+	for(int i=1; i < MAX_R; ++i) {        // i = 가장 바깥원의 반지름
 		dp[i][0] = 1;
 		pSum[i][0] = 1;
 
-		for(int j=1; j<MAX_COLOR; ++j) {
+		for(int j=1; j<MAX_COLOR; ++j) {    // j = 사용한 파랑 페인트 양
 
-			if(j <= i*(i+1)/2) {
+			if(j <= i*(i+1)/2) {              // j -> [0 ... i*(i+1)/2]
 				dp[i][j] = dp[i-1][j];
 				if(j >= i) dp[i][j] += dp[i-1][j-i];
 
@@ -38,11 +39,11 @@ int main()
 		int a, b, r, ans = 0;
 		cin >> a >> b;
 
-		for(r=1; r < MAX_R; ++r) {
+		for(r=1; r < MAX_R; ++r) {     // r = 가장 바깥원의 반지름
 			int a2 = r*(r+1)/2 - a, sum;
 
-			a2 = max(a2, 0);
-			if(a2 > b) break;
+			a2 = max(a2, 0);             // a2 = 필요한 파랑 물약의 최소양
+			if(a2 > b) break;           
 
 			sum = (pSum[r][b] - pSum[r][a2] + MOD) % MOD;
 			sum = (sum + dp[r][a2]) % MOD;
